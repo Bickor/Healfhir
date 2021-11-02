2e0682c64ac304782e7e95f979af88d4842d64b9
Martin Heberling <martin.heberling.dalies@gmail.com>
2021-11-02 00:57:48 -0400

    Created React app and Springboot app.

---
+++ src/main/java/com/juniordesign/team1122/Team1122Application.java
@@ -0,0 +1,13 @@
+package com.juniordesign.team1122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team1122Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/juniordesign/team1122/Team1122ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniordesign.team1122;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1122ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
