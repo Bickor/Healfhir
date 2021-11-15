0f09fce467f6c42668518af8df9f1de628e076ff
noheliadasilva <nohelia.dasilva@gatech.edu>
2021-11-15 15:29:15 -0500

    new patient page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,57 @@ import { useParams } from 'react-router-dom';
 
 const patients = data;
 
+function NewPatient() {
+    return (
+        <div>
+            <Link to="/">
+                <Button
+                    variant="btn btn-success"
+                    style={{display: "flex", alignItems:'center', justifyContent:'center', marginLeft: 50, marginTop: 50}}>
+                        Home page
+                </Button>
+            </Link>
+
+            <h1 style={{marginLeft:580, marginBottom: 60}}>
+                Add new patient
+            </h1>
+
+            <form>
+                <label style={{marginLeft:50, alignSelf:"center"}}>
+                    First Name:
+                    <input type="text" name="name"/>
+                </label>
+                
+                <label style={{marginLeft:50,alignSelf:"center"}}>
+                    Last Name:
+                    <input type="text" name="name" />
+                </label>
+
+                <label style={{marginLeft:50, alignSelf:"center"}}
+                    htmlFor="gender">Gender:
+                </label>
+                <select id="gender" name="gender">
+                    <option value="Female">Female</option>
+                    <option value="Male">Male</option>
+                    <option value="Other">Other</option>
+                </select>
+
+                <button style={{marginLeft:50, alignSelf:"center"}}>Submit</button>
+            </form>
+
+        </div>
+    )
+}
+
 function App() {
 
   return (
 
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/patient/:slug" element={<Patient/>}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/patient/:slug" element={<Patient/>}/>
+          <Route path="/newPatient" element={<NewPatient/>}/>
       </Routes>
     </Router>
     
@@ -59,11 +102,13 @@ function Home() {
       </div>
 
       <form>
+          <Link to={"/newPatient"}>
             <Button
                 variant="btn btn-success"
                 style={{display: "flex", alignItems:'center', justifyContent:'center', marginLeft: 580, marginTop: 250}}>
                 Add new patient
             </Button>
+          </Link>
       </form>
 
     </div>
@@ -85,18 +130,19 @@ function Patient() {
           <Link to="/">
               <Button
                   variant="btn btn-success"
-                  style={{display: "flex", alignItems:'center', justifyContent:'left', marginLeft: 50, marginTop: 50}}>
+                  style={{display: "flex", alignItems:'center', justifyContent:'center', marginLeft: 50, marginTop: 50}}>
                       Home page
               </Button>
           </Link>
 
-          <h1>
+
+          <h1 style={{marginLeft:580, marginBottom: 60}}>
               Patient Information   
           </h1>
 
-          <h3> first: {currPatient.name.given} </h3>
-          <h3> last: {currPatient.name.family} </h3>
-          <p> address: {currPatient.address[0].text} </p>
+          <h3 style={{marginLeft:50}}> First Name: {currPatient.name.given} </h3>
+          <h3 style={{marginLeft:50}}> Lastname: {currPatient.name.family} </h3>
+          <p style={{marginLeft:50}}> Address: {currPatient.address[0].text} </p>
 
       </div>
   );
